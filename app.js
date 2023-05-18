eabbe36c578c2bbb9b9a836238e75e28d1df5371
sahanenes <sahanenesss@gmail.com>
2023-05-18 18:04:22 +0300

    session

--- app.js
+++ app.js
@@ -3,24 +3,44 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+require("dotenv").config();
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
-require("dotenv").config();
+
+const MONGODB_URI = `${process.env.URI}`;
 
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
 
 app.use((req, res, next) => {
-  User.findById("6463fcd9d6ca6288d6d05317")
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then((user) => {
       req.user = user;
       next();
@@ -30,17 +50,18 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
-const uri = process.env.URI;
+
 mongoose
-  .connect(`${uri}`, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(MONGODB_URI)
   .then((result) => {
     User.findOne().then((user) => {
       if (!user) {
         const user = new User({
-          name: "Enes",
-          email: "sahanenesss@gmail.com",
+          name: "Max",
+          email: "max@test.com",
           cart: {
             items: [],
           },
@@ -48,7 +69,6 @@ mongoose
         user.save();
       }
     });
-
     app.listen(3000);
   })
   .catch((err) => {
